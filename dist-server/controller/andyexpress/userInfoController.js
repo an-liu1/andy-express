46c4096048610d6dbce185b7ec749085232f0b8e
an-liu1 <36012691+an-liu1@users.noreply.github.com>
2020-04-14 13:46:08 -0400

    2

--- dist-server/controller/andyexpress/userInfoController.js
+++ dist-server/controller/andyexpress/userInfoController.js
@@ -48,7 +48,7 @@ userInfoController.avatarUpload = function (req, res) {
   var base64Data = avatar.replace(/^data:image\/\w+;base64,/, "");
   var dataBuffer = Buffer.from(base64Data, "base64");
   var time = Date.now();
-  var imagePath = "images/andyexpress/avatar/".concat(req.user.id, "_").concat(time, ".png");
+  var imagePath = "images/andyexpress/avatar/".concat(time, ".png");
 
   _fs["default"].writeFile("public/".concat(imagePath), dataBuffer, function (err, written, buffer) {
     console.log(err);
--- server/controller/andyexpress/userInfoController.js
+++ server/controller/andyexpress/userInfoController.js
@@ -37,7 +37,7 @@ userInfoController.avatarUpload = (req, res) => {
   var base64Data = avatar.replace(/^data:image\/\w+;base64,/, "");
   var dataBuffer = Buffer.from(base64Data, "base64");
   let time = Date.now();
-  let imagePath = `images/andyexpress/avatar/${req.user.id}_${time}.png`;
+  let imagePath = `images/andyexpress/avatar/${time}.png`;
   fs.writeFile(`public/${imagePath}`, dataBuffer, function (err, written, buffer) {
     console.log(err)
     console.log(written)
